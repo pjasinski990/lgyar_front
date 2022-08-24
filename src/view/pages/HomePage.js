b270c7a75f5c799bea95c52869eb6355003bd060
Piotr Jasinski <piotr.jasinski990@gmail.com>
2022-08-24 19:20:55 +0200

    envelopes wip

--- src/view/pages/HomePage.js
+++ src/view/pages/HomePage.js
@@ -1,7 +1,7 @@
 import {toast} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css'
 import React, {useState} from "react";
-import {Container, InputGroup} from "react-bootstrap";
+import {Container} from "react-bootstrap";
 import {Link} from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
@@ -12,6 +12,8 @@ import CurrencyInput from "react-currency-input-field";
 import {makeBackendRequest} from "../../util";
 import {defaultEnvelopes} from "../../res/defaultEnvelopes";
 import dateFormat from "dateformat";
+import * as PropTypes from "prop-types";
+import ProgressBar from 'react-js-progressbar'
 
 function HomeNonLogged(props) {
     return (
@@ -24,19 +26,57 @@ function HomeNonLogged(props) {
     )
 }
 
+function Transaction(props) {
+    Transaction.propTypes = {
+        value: PropTypes.string.isRequired,
+        category: PropTypes.string.isRequired,
+        timestamp: PropTypes.string.isRequired
+    }
+
+    let type;
+    let value;
+    const category = props.category
+    if (props.value.startsWith('-')) {
+        type = 'expense'
+        value = props.value.substr(1)
+    }
+    else {
+        type = 'income'
+        value = props.value
+    }
+
+    return (
+        <div className={`text-mono m-2 p-2 px-4 ${type === 'income' ? 'transaction-income' : 'transaction-expense'}`}>
+            <Row>
+                <Col xs={'5'}>
+                    {props.timestamp}
+                </Col>
+                <Col xs={'3'}>
+                    {category}
+                </Col>
+                <Col xs={'4'} style={{textAlign: 'right'}}>
+                    {value}
+                </Col>
+            </Row>
+        </div>
+    )
+}
+
 function ActivePeriodTransactions(props) {
     const transactions = props.user.activePeriod.transactions
     if (transactions === null || transactions.length === 0) {
-        return <Container className={'content-container'}>
+        return <h4 className={'mx-3'}>
             No transactions yet... Create one using the form below
-        </Container>
+        </h4>
     }
     else {
-        const result = transactions.map(t => <p key={t.timestamp}>from {t.timestamp}: {t.category} {t.balanceDifference}</p>)
+        const result = transactions.map(t =>
+            <Transaction key={t.timestamp} timestamp={t.timestamp} category={t.category} value={t.balanceDifference}/>
+        )
         return (
-            <Container className={'content-container'}>
+            <div>
                 {result}
-            </Container>
+            </div>
         )
     }
 }
@@ -67,16 +107,6 @@ function AddNewTransactionButton(props) {
         }
     }
 
-    if (props.user.activePeriod === null) {
-        return <Container className={'content-container'}>
-            <h3>
-                There is no active budgeting period
-            </h3>
-            <p>
-                Activate a budgeting period from the archive or create a new period using the button below
-            </p>
-        </Container>
-    }
     const envelopes = props.user.activePeriod.envelopes
     const options = envelopes.map(e => {
         return <option key={e.categoryName}>{e.categoryName}</option>
@@ -86,9 +116,11 @@ function AddNewTransactionButton(props) {
         e.preventDefault()
         if (transactionCategory === '') {
             toast.error('Select an envelope for this transaction')
+            return
         }
         else if (transactionValue === '') {
             toast.error('Input a value for this transaction')
+            return
         }
 
         console.log(transactionCategory, ' ', transactionType, ' ', transactionValue)
@@ -99,13 +131,12 @@ function AddNewTransactionButton(props) {
         const body = JSON.stringify(newTransaction)
         makeBackendRequest('ap/new_transaction', 'post', body, headers)
             .then(res => {
-                console.log(res)
                 window.location.reload(false)
             })
     }
 
     return (
-        <Container className={'content-container'}>
+        <div className={'px-0 py-3'}>
             <Form className={'text-mono'} onSubmit={addTransaction}>
                 <Stack direction={'horizontal'}>
                     <Form.Select value={transactionCategory} onChange={handleTransactionCategoryChange} className={'mx-1'}>
@@ -117,10 +148,11 @@ function AddNewTransactionButton(props) {
                         <option value={'income'}>Income</option>
                     </Form.Select>
                     <CurrencyInput
-                        style={{boxShadow: '0 0 1px red'}}
+                        style={{boxShadow: '0 0 1px red', height: '34px'}}
                         id={'transactionValueInput'}
                         disableGroupSeparators={true}
                         allowNegativeValue={false}
+                        decimalScale={2}
                         placeholder={'0.00'}
                         className={'mx-1'}
                         onValueChange={handleTransactionValueChange}
@@ -128,8 +160,56 @@ function AddNewTransactionButton(props) {
                     <Button type={'submit'} className={'mx-1'}>Add</Button>
                 </Stack>
             </Form>
-        </Container>
+        </div>
     )
+}
+
+function Envelope(props) {
+    Envelope.propTypes = {
+        envelopeName: PropTypes.string.isRequired,
+        spent: PropTypes.number.isRequired,
+        limit: PropTypes.number.isRequired,
+    }
+
+    const progress = props.limit > 0 ? props.spent * 100 / props.limit : 0
+    return <ProgressBar input={progress}
+                        pathColor={['#125521', 'green']}
+                        shape={'arc'}
+                        clockwise={false}
+                        trailWidth={42}
+                        pathWidth={50}
+    >
+        {props.envelopeName}
+    </ProgressBar>
+}
+
+function EnvelopeContainer(props) {
+    EnvelopeContainer.propTypes = {
+        user: PropTypes.object.isRequired,
+        // TODO implement variable cols
+        // nCols: PropTypes.number.isRequired
+    }
+
+    let envelopes;
+    if (!!props.user.activePeriod) {
+        envelopes = props.user.activePeriod.envelopes.map((e) => {
+            const nSpent = Number(e.spent)
+            const nLimit = Number(e.limit)
+            return <Envelope key={e.categoryName} envelopeName={e.categoryName} spent={nSpent} limit={nLimit}/>
+        })
+
+    let res = []
+    for (let i = 0; i < envelopes.length; i += 2) {
+        res.push(
+            <Row>
+                <Col xs={6}>{envelopes[i]}</Col>
+                {i < envelopes.length - 1 && <Col>{envelopes[i+1]}</Col>}
+            </Row>
+        )
+    }
+
+    return <div className={'d-grid'}>{res}</div>
+    }
 }
 
 function HomeLogged(props) {
@@ -152,14 +232,8 @@ function HomeLogged(props) {
             }
             makeBackendRequest('ap/create', 'post', body, headers)
                 .then(res => {
-                        console.log(res)
-                        res.json()
-                            .then(data => {
-                                console.log(data)
-                                window.location.reload(false)
-                            })
-                    }
-                )
+                    window.location.reload(false)
+                })
         }
     }
 
@@ -170,7 +244,6 @@ function HomeLogged(props) {
         else {
             makeBackendRequest('ap/archive', 'post', null, null)
                 .then(res => {
-                    console.log(res)
                     window.location.reload(false)
                 })
         }
@@ -189,18 +262,33 @@ function HomeLogged(props) {
         <>
             <h1 className={'pt-3 text-mono'} style={{display: 'flex', justifyContent: 'center'}}>{tooltip}</h1>
             <Container className={'d-grid'}>
-                <Row>
-                    <Col xs={8}>
-                        {props.user.activePeriod && <ActivePeriodTransactions user={props.user}/>}
-                        <AddNewTransactionButton user={props.user}/>
-                    </Col>
-                    <Col>
-                        <Container className={'content-container'}>
-                            <h1>Hello world!</h1>
-                            <p>Add envelope icons here</p>
-                        </Container>
-                    </Col>
-                </Row>
+                {!props.user.activePeriod &&
+                    <Container className={'content-container'}>
+                        <h3>
+                            There is no active budgeting period
+                        </h3>
+                        <p>
+                            Activate a budgeting period from the archive or create a new period using the button below
+                        </p>
+                    </Container>
+                }
+                {props.user.activePeriod &&
+                    <Row>
+                        <Col xs={8}>
+                            <Container className={'content-container'}>
+                                <h3 className={'mb-3 mx-2'}>Transactions</h3>
+                                <ActivePeriodTransactions user={props.user}/>
+                                <AddNewTransactionButton user={props.user}/>
+                            </Container>
+                        </Col>
+                        <Col>
+                            <Container id={'envelope-container'} className={'content-container'}>
+                                <h3>Envelopes</h3>
+                                <EnvelopeContainer user={props.user} nCols={2}/>
+                            </Container>
+                        </Col>
+                    </Row>
+                }
                 <Row className={'px-1 mt-3'}>
                     <Col xs={6} className={'px-0'}>
                         <div className={'m-2 d-grid px-0'}>
